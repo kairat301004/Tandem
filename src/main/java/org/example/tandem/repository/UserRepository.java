400c3f08c2daac96ec166ce211db8da9544c48e9
kairat <kairat301004@gmail.com>
2026-05-03 13:29:59 +0300

    create user profile

--- src/main/java/org/example/tandem/repository/UserRepository.java
+++ src/main/java/org/example/tandem/repository/UserRepository.java
@@ -6,16 +6,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
-    // Новый метод с принудительной загрузкой ролей
-    @Query("SELECT u FROM User u " +
-            "LEFT JOIN FETCH u.roles r " +
-            "LEFT JOIN FETCH r.permissions " +
-            "WHERE u.email = :email")
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles r LEFT JOIN FETCH r.permissions WHERE u.email = :email")
     Optional<User> findByEmailWithRoles(@Param("email") String email);
+
+    // Поиск по имени, фамилии или email
+    List<User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrEmailContainingIgnoreCase(
+            String firstName, String lastName, String email);
 }
